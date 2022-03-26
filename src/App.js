2104fa4209ad767d92e99da3b9afa899b9e0677a
VladanS10 <vladan.nish@gmail.com>
2022-03-26 20:56:08 +0100

    izmene

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function App() {
@@ -6,23 +6,54 @@ function App() {
   const [data5, setData5] = useState([]);
   const [fiveDays, setFiveDays] = useState(1)
   const [location, setLocation] = useState('')
-
+  const [backgroundImage, setBackgroundImage] = useState('./assets/R.jpg')
+  const [color, setColor] = useState('white')
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=d34215484337d226374b727d251f1608` 
   const url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=d34215484337d226374b727d251f1608`
 
+
   const smallCard = (day,index) =>{
     console.log(day);
+    const icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     return(
+
       <div className='daily' key={index}>
+        <img src={icon} />
           {getDayName(day.dt)}
         <p>
           {day.dt_txt.slice(0, 10)}
           
-        </p>
-          {day.main.temp_max}&#176;C
+        </p>Max-temp: 
+          {Math.floor(day.main.temp_max)}&#176;C
       </div>
     )
   } 
+  useEffect(() =>{
+    if(data.weather != undefined){
+      switch (data.weather[0].main){
+        case 'Clear':
+          setBackgroundImage('./assets/sunny.jpg')
+          setColor('white') 
+          break;
+        case 'Clouds':
+          setBackgroundImage('./assets/sunset.jpg')
+          setColor('white') 
+          break;
+        case 'Rain':
+          setBackgroundImage('./assets/rain.jpg')
+          setColor('white') 
+          break;
+        case 'Snow':
+        setBackgroundImage('./assets/snow.jpg')
+        setColor('black') 
+        break;
+          default:
+            setBackgroundImage('./assets/R.jpg')
+            setColor('white') 
+            break;
+          }
+      }
+  }, [data])
 
   const getDayName = (timeStamp) =>{
     var xx = new Date();
@@ -38,7 +69,7 @@ function App() {
         return "Thuesday";
       break;
       case 3:
-        return "Wensday";
+        return "Wednesday";
       break;
       case 4:
         return "Thursday";
@@ -88,19 +119,18 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" id='mainBlock' style={{background: `url(${backgroundImage})`, color}} key={backgroundImage}>
       <div className="search">
-        <input 
+        <input
+        style={{color}} 
         value={location}
         onChange={event => {
           setLocation(event.target.value)
           setFiveDays(fiveDays + 1)}
         }
-
         onKeyPress={search}
         placeholder="Enter Location"
         type="text"/>
-        
       </div>
       <div className="container">
         <div className="top">
@@ -108,7 +138,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp}&#176;C</h1> : null}
+            {data.main ? <h1>{Math.floor(data.main.temp)}&#176;C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -120,7 +150,7 @@ function App() {
         {data.name != undefined &&
         <div className="bottom">
            <div className="feels">
-             {data.main ? <p className='bold'>{data.main.feels_like}</p> : null}
+             {data.main ? <p className='bold'>{Math.floor(data.main.feels_like)}&#176;C</p> : null}
              <p>Feels Like</p>
            </div>
            <div className="humidity">
